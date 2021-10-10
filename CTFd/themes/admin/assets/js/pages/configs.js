b8ad07decd4bd0e085e2298276756d1e42dc308b
ahadley1124 <49847299+ahadley1124@users.noreply.github.com>
2021-10-09 22:31:56 -0400

    Add files via upload

--- CTFd/themes/admin/assets/js/pages/configs.js
+++ CTFd/themes/admin/assets/js/pages/configs.js
@@ -1,23 +1,34 @@
 import "./main";
 import "core/utils";
 import "bootstrap/js/dist/tab";
-import Moment from "moment-timezone";
-import moment from "moment-timezone";
+import dayjs from "dayjs";
+import advancedFormat from "dayjs/plugin/advancedFormat";
+import utc from "dayjs/plugin/utc";
+import timezone from "dayjs/plugin/timezone";
+import timezones from "../timezones";
 import CTFd from "core/CTFd";
 import { default as helpers } from "core/helpers";
 import $ from "jquery";
-import { ezQuery, ezProgressBar } from "core/ezq";
+import { ezQuery, ezProgressBar, ezAlert } from "core/ezq";
+import CodeMirror from "codemirror";
+import "codemirror/mode/htmlmixed/htmlmixed.js";
+import Vue from "vue/dist/vue.esm.browser";
+import FieldList from "../components/configs/fields/FieldList.vue";
+
+dayjs.extend(advancedFormat);
+dayjs.extend(utc);
+dayjs.extend(timezone);
 
 function loadTimestamp(place, timestamp) {
   if (typeof timestamp == "string") {
-    timestamp = parseInt(timestamp, 10);
+    timestamp = parseInt(timestamp, 10) * 1000;
   }
-  const m = Moment(timestamp * 1000);
-  $("#" + place + "-month").val(m.month() + 1); // Months are zero indexed (http://momentjs.com/docs/#/get-set/month/)
-  $("#" + place + "-day").val(m.date());
-  $("#" + place + "-year").val(m.year());
-  $("#" + place + "-hour").val(m.hour());
-  $("#" + place + "-minute").val(m.minute());
+  const d = dayjs(timestamp);
+  $("#" + place + "-month").val(d.month() + 1); // Months are zero indexed (https://day.js.org/docs/en/get-set/month)
+  $("#" + place + "-day").val(d.date());
+  $("#" + place + "-year").val(d.year());
+  $("#" + place + "-hour").val(d.hour());
+  $("#" + place + "-minute").val(d.minute());
   loadDateValues(place);
 }
 
@@ -27,21 +38,21 @@ function loadDateValues(place) {
   const year = $("#" + place + "-year").val();
   const hour = $("#" + place + "-hour").val();
   const minute = $("#" + place + "-minute").val();
-  const timezone = $("#" + place + "-timezone").val();
+  const timezone_string = $("#" + place + "-timezone").val();
 
   const utc = convertDateToMoment(month, day, year, hour, minute);
-  if (isNaN(utc.unix())) {
+  if (utc.unix() && month && day && year && hour && minute) {
+    $("#" + place).val(utc.unix());
+    $("#" + place + "-local").val(
+      utc.format("dddd, MMMM Do YYYY, h:mm:ss a z (zzz)")
+    );
+    $("#" + place + "-zonetime").val(
+      utc.tz(timezone_string).format("dddd, MMMM Do YYYY, h:mm:ss a z (zzz)")
+    );
+  } else {
     $("#" + place).val("");
     $("#" + place + "-local").val("");
     $("#" + place + "-zonetime").val("");
-  } else {
-    $("#" + place).val(utc.unix());
-    $("#" + place + "-local").val(
-      utc.local().format("dddd, MMMM Do YYYY, h:mm:ss a zz")
-    );
-    $("#" + place + "-zonetime").val(
-      utc.tz(timezone).format("dddd, MMMM Do YYYY, h:mm:ss a zz")
-    );
   }
 }
 
@@ -78,7 +89,7 @@ function convertDateToMoment(month, day, year, hour, minute) {
     ":" +
     min_str +
     ":00";
-  return Moment(date_string, Moment.ISO_8601);
+  return dayjs(date_string);
 }
 
 function updateConfigs(event) {
@@ -108,8 +119,17 @@ function updateConfigs(event) {
     }
   });
 
-  CTFd.api.patch_config_list({}, params).then(response => {
-    window.location.reload();
+  CTFd.api.patch_config_list({}, params).then(function(_response) {
+    if (_response.success) {
+      window.location.reload();
+    } else {
+      let errors = _response.errors.value.join("\n");
+      ezAlert({
+        title: "Error!",
+        body: errors,
+        button: "Okay"
+      });
+    }
   });
 }
 
@@ -142,6 +162,27 @@ function uploadLogo(event) {
   });
 }
 
+function switchUserMode(event) {
+  event.preventDefault();
+  if (
+    confirm(
+      "Are you sure you'd like to switch user modes?\n\nAll user submissions, awards, unlocks, and tracking will be deleted!"
+    )
+  ) {
+    let formData = new FormData();
+    formData.append("submissions", true);
+    formData.append("nonce", CTFd.config.csrfNonce);
+    fetch(CTFd.config.urlRoot + "/admin/reset", {
+      method: "POST",
+      credentials: "same-origin",
+      body: formData
+    });
+    // Bind `this` so that we can reuse the updateConfigs function
+    let binded = updateConfigs.bind(this);
+    binded(event);
+  }
+}
+
 function removeLogo() {
   ezQuery({
     title: "Remove logo",
@@ -152,9 +193,126 @@ function removeLogo() {
       };
       CTFd.api
         .patch_config({ configKey: "ctf_logo" }, params)
-        .then(response => {
+        .then(_response => {
           window.location.reload();
         });
+    }
+  });
+}
+
+function smallIconUpload(event) {
+  event.preventDefault();
+  let form = event.target;
+  helpers.files.upload(form, {}, function(response) {
+    const f = response.data[0];
+    const params = {
+      value: f.location
+    };
+    CTFd.fetch("/api/v1/configs/ctf_small_icon", {
+      method: "PATCH",
+      body: JSON.stringify(params)
+    })
+      .then(function(response) {
+        return response.json();
+      })
+      .then(function(response) {
+        if (response.success) {
+          window.location.reload();
+        } else {
+          ezAlert({
+            title: "Error!",
+            body: "Icon uploading failed!",
+            button: "Okay"
+          });
+        }
+      });
+  });
+}
+
+function removeSmallIcon() {
+  ezQuery({
+    title: "Remove logo",
+    body: "Are you sure you'd like to remove the small site icon?",
+    success: function() {
+      const params = {
+        value: null
+      };
+      CTFd.api
+        .patch_config({ configKey: "ctf_small_icon" }, params)
+        .then(_response => {
+          window.location.reload();
+        });
+    }
+  });
+}
+
+function importCSV(event) {
+  event.preventDefault();
+  let csv_file = document.getElementById("import-csv-file").files[0];
+  let csv_type = document.getElementById("import-csv-type").value;
+
+  let form_data = new FormData();
+  form_data.append("csv_file", csv_file);
+  form_data.append("csv_type", csv_type);
+  form_data.append("nonce", CTFd.config.csrfNonce);
+
+  let pg = ezProgressBar({
+    width: 0,
+    title: "Upload Progress"
+  });
+
+  $.ajax({
+    url: CTFd.config.urlRoot + "/admin/import/csv",
+    type: "POST",
+    data: form_data,
+    processData: false,
+    contentType: false,
+    statusCode: {
+      500: function(resp) {
+        // Normalize errors
+        let errors = JSON.parse(resp.responseText);
+        let errorText = "";
+        errors.forEach(element => {
+          errorText += `Line ${element[0]}: ${JSON.stringify(element[1])}\n`;
+        });
+
+        // Show errors
+        alert(errorText);
+
+        // Hide progress modal if its there
+        pg = ezProgressBar({
+          target: pg,
+          width: 100
+        });
+        setTimeout(function() {
+          pg.modal("hide");
+        }, 500);
+      }
+    },
+    xhr: function() {
+      let xhr = $.ajaxSettings.xhr();
+      xhr.upload.onprogress = function(e) {
+        if (e.lengthComputable) {
+          let width = (e.loaded / e.total) * 100;
+          pg = ezProgressBar({
+            target: pg,
+            width: width
+          });
+        }
+      };
+      return xhr;
+    },
+    success: function(_data) {
+      pg = ezProgressBar({
+        target: pg,
+        width: 100
+      });
+      setTimeout(function() {
+        pg.modal("hide");
+      }, 500);
+      setTimeout(function() {
+        window.location.reload();
+      }, 700);
     }
   });
 }
@@ -180,7 +338,6 @@ function importConfig(event) {
     contentType: false,
     statusCode: {
       500: function(resp) {
-        console.log(resp.responseText);
         alert(resp.responseText);
       }
     },
@@ -197,7 +354,7 @@ function importConfig(event) {
       };
       return xhr;
     },
-    success: function(data) {
+    success: function(_data) {
       pg = ezProgressBar({
         target: pg,
         width: 100
@@ -214,18 +371,13 @@ function importConfig(event) {
 
 function exportConfig(event) {
   event.preventDefault();
-  const href = CTFd.config.urlRoot + "/admin/export";
   window.location.href = $(this).attr("href");
 }
 
-function showTab(event) {
-  window.location.hash = this.hash;
-}
-
 function insertTimezones(target) {
-  let current = $("<option>").text(moment.tz.guess());
+  let current = $("<option>").text(dayjs.tz.guess());
   $(target).append(current);
-  let tz_names = moment.tz.names();
+  let tz_names = timezones;
   for (let i = 0; i < tz_names.length; i++) {
     let tz = $("<option>").text(tz_names[i]);
     $(target).append(tz);
@@ -233,19 +385,109 @@ function insertTimezones(target) {
 }
 
 $(() => {
+  const theme_header_editor = CodeMirror.fromTextArea(
+    document.getElementById("theme-header"),
+    {
+      lineNumbers: true,
+      lineWrapping: true,
+      mode: "htmlmixed",
+      htmlMode: true
+    }
+  );
+
+  const theme_footer_editor = CodeMirror.fromTextArea(
+    document.getElementById("theme-footer"),
+    {
+      lineNumbers: true,
+      lineWrapping: true,
+      mode: "htmlmixed",
+      htmlMode: true
+    }
+  );
+
+  const theme_settings_editor = CodeMirror.fromTextArea(
+    document.getElementById("theme-settings"),
+    {
+      lineNumbers: true,
+      lineWrapping: true,
+      mode: { name: "javascript", json: true }
+    }
+  );
+
+  // Handle refreshing codemirror when switching tabs.
+  // Better than the autorefresh approach b/c there's no flicker
+  $("a[href='#theme']").on("shown.bs.tab", function(_e) {
+    theme_header_editor.refresh();
+    theme_footer_editor.refresh();
+    theme_settings_editor.refresh();
+  });
+
+  $(
+    "a[href='#legal'], a[href='#tos-config'], a[href='#privacy-policy-config']"
+  ).on("shown.bs.tab", function(_e) {
+    $("#tos-config .CodeMirror").each(function(i, el) {
+      el.CodeMirror.refresh();
+    });
+    $("#privacy-policy-config .CodeMirror").each(function(i, el) {
+      el.CodeMirror.refresh();
+    });
+  });
+
+  $("#theme-settings-modal form").submit(function(e) {
+    e.preventDefault();
+    theme_settings_editor
+      .getDoc()
+      .setValue(JSON.stringify($(this).serializeJSON(), null, 2));
+    $("#theme-settings-modal").modal("hide");
+  });
+
+  $("#theme-settings-button").click(function() {
+    let form = $("#theme-settings-modal form");
+    let data;
+
+    // Ignore invalid JSON data
+    try {
+      data = JSON.parse(theme_settings_editor.getValue());
+    } catch (e) {
+      data = {};
+    }
+
+    $.each(data, function(key, value) {
+      var ctrl = form.find(`[name='${key}']`);
+      switch (ctrl.prop("type")) {
+        case "radio":
+        case "checkbox":
+          ctrl.each(function() {
+            if ($(this).attr("value") == value) {
+              $(this).attr("checked", value);
+            }
+          });
+          break;
+        default:
+          ctrl.val(value);
+      }
+    });
+    $("#theme-settings-modal").modal();
+  });
+
   insertTimezones($("#start-timezone"));
   insertTimezones($("#end-timezone"));
   insertTimezones($("#freeze-timezone"));
 
-  $(".config-section > form:not(.form-upload)").submit(updateConfigs);
+  $(".config-section > form:not(.form-upload, .custom-config-form)").submit(
+    updateConfigs
+  );
   $("#logo-upload").submit(uploadLogo);
+  $("#user-mode-form").submit(switchUserMode);
   $("#remove-logo").click(removeLogo);
+  $("#ctf-small-icon-upload").submit(smallIconUpload);
+  $("#remove-small-icon").click(removeSmallIcon);
   $("#export-button").click(exportConfig);
   $("#import-button").click(importConfig);
-  $(".nav-pills a").click(showTab);
+  $("#import-csv-form").submit(importCSV);
   $("#config-color-update").click(function() {
     const hex_code = $("#config-color-picker").val();
-    const user_css = $("#theme-header").val();
+    const user_css = theme_header_editor.getValue();
     let new_css;
     if (user_css.length) {
       let css_vars = `theme-color: ${hex_code};`;
@@ -258,7 +500,7 @@ $(() => {
         `.jumbotron{background-color: var(--theme-color) !important;}\n` +
         `</style>\n`;
     }
-    $("#theme-header").val(new_css);
+    theme_header_editor.getDoc().setValue(new_css);
   });
 
   $(".start-date").change(function() {
@@ -270,12 +512,6 @@ $(() => {
   $(".freeze-date").change(function() {
     loadDateValues("freeze");
   });
-
-  let hash = window.location.hash;
-  if (hash) {
-    hash = hash.replace("<>[]'\"", "");
-    $('ul.nav a[href="' + hash + '"]').tab("show");
-  }
 
   const start = $("#start").val();
   const end = $("#end").val();
@@ -297,4 +533,23 @@ $(() => {
       $("#mail_username_password").toggle(this.checked);
     })
     .change();
+
+  // Insert FieldList element for users
+  const fieldList = Vue.extend(FieldList);
+  let userVueContainer = document.createElement("div");
+  document.querySelector("#user-field-list").appendChild(userVueContainer);
+  new fieldList({
+    propsData: {
+      type: "user"
+    }
+  }).$mount(userVueContainer);
+
+  // Insert FieldList element for teams
+  let teamVueContainer = document.createElement("div");
+  document.querySelector("#team-field-list").appendChild(teamVueContainer);
+  new fieldList({
+    propsData: {
+      type: "team"
+    }
+  }).$mount(teamVueContainer);
 });
