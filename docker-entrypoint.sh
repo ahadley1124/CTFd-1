6929301beb8df67c8f15d68e52644298a30593e3
ahadley1124 <49847299+ahadley1124@users.noreply.github.com>
2021-10-09 22:40:08 -0400

    Add files via upload

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,11 +1,12 @@
-#!/bin/sh
-set -eo pipefail
+#!/bin/bash
+set -euo pipefail
 
 WORKERS=${WORKERS:-1}
 WORKER_CLASS=${WORKER_CLASS:-gevent}
 ACCESS_LOG=${ACCESS_LOG:--}
 ERROR_LOG=${ERROR_LOG:--}
 WORKER_TEMP_DIR=${WORKER_TEMP_DIR:-/dev/shm}
+SECRET_KEY=${SECRET_KEY:-}
 
 # Check that a .ctfd_secret_key file or SECRET_KEY envvar is set
 if [ ! -f .ctfd_secret_key ] && [ -z "$SECRET_KEY" ]; then
@@ -17,22 +18,8 @@ if [ ! -f .ctfd_secret_key ] && [ -z "$SECRET_KEY" ]; then
     fi
 fi
 
-# Check that the database is available
-if [ -n "$DATABASE_URL" ]
-    then
-    url=`echo $DATABASE_URL | awk -F[@//] '{print $4}'`
-    database=`echo $url | awk -F[:] '{print $1}'`
-    port=`echo $url | awk -F[:] '{print $2}'`
-    echo "Waiting for $database:$port to be ready"
-    while ! mysqladmin ping -h "$database" -P "$port" --silent; do
-        # Show some progress
-        echo -n '.';
-        sleep 1;
-    done
-    echo "$database is ready"
-    # Give it another second.
-    sleep 1;
-fi
+# Ensures that the database is available
+python ping.py
 
 # Initialize database
 python manage.py db upgrade
--- export.py
+++ export.py
@@ -9,6 +9,9 @@ import shutil
 
 app = create_app()
 with app.app_context():
+    print(
+        "This file will be deleted in CTFd v4.0. Switch to using `python manage.py export_ctf`"
+    )
     backup = export_ctf()
 
     if len(sys.argv) > 1:
@@ -16,7 +19,7 @@ with app.app_context():
             shutil.copyfileobj(backup, target)
     else:
         ctf_name = config.ctf_name()
-        day = datetime.datetime.now().strftime("%Y-%m-%d")
+        day = datetime.datetime.now().strftime("%Y-%m-%d_%T")
         full_name = "{}.{}.zip".format(ctf_name, day)
 
         with open(full_name, "wb") as target:
